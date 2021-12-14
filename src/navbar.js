06325b76450b057873aa2d709fc50ea20a9df541
bomendez <mendez.bo@husky.neu.edu>
2021-12-13 22:23:50 -0800

    fixed navbar formatting for favorites

--- src/navbar.js
+++ src/navbar.js
@@ -18,11 +18,11 @@ function NavBar() {
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
+        <li class="nav-item">
           <a class="nav-link" href="/">Home</a>
         </li>
-        <li class="nav-item">
-        <Link class="text-muted" to={{pathname: "/myFavorites"}}>Favorites</Link>
+        <li class="nav-item align-bottom">
+          <Link class="nav-link" to={{pathname: "/myFavorites"}}>Favorites</Link>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
